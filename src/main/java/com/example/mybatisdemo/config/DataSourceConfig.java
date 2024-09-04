25f6118e208567bf4997e6f3b7bbf88eb4c74022
H.PALMAN <hpalman@gmail.com>
2024-09-04 17:44:58 +0900

    modify

--- src/main/java/com/example/mybatisdemo/config/DataSourceConfig.java
+++ src/main/java/com/example/mybatisdemo/config/DataSourceConfig.java
@@ -1,34 +1,40 @@
 package com.example.mybatisdemo.config;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class DataSourceConfig {
 
     // Schema 1 DataSource Configuration
     @Primary
     @Bean(name = "schema1DataSource")
     @ConfigurationProperties(prefix = "spring.datasource.schema1")
-    public DataSource schema1DataSource() {
+    DataSource schema1DataSource() {
+    	log.info("called");
         return org.springframework.boot.jdbc.DataSourceBuilder.create().build();
     }
 
     @Primary
     @Bean(name = "schema1SqlSessionFactory")
-    public SqlSessionFactory schema1SqlSessionFactory(@Qualifier("schema1DataSource") DataSource dataSource) throws Exception {
+    SqlSessionFactory schema1SqlSessionFactory(@Qualifier("schema1DataSource") DataSource dataSource) throws Exception {
+    	log.info("called");
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setConfigLocation(new ClassPathResource("/mybatis-config.xml"));
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/schema1/*.xml"));
         return sessionFactory.getObject();
@@ -36,38 +42,47 @@ public class DataSourceConfig {
 
     @Primary
     @Bean(name = "schema1TransactionManager")
-    public DataSourceTransactionManager schema1TransactionManager(@Qualifier("schema1DataSource") DataSource dataSource) {
+    DataSourceTransactionManager schema1TransactionManager(@Qualifier("schema1DataSource") DataSource dataSource) {
+    	log.info("called");
         return new DataSourceTransactionManager(dataSource);
     }
 
     @Primary
     @Bean(name = "schema1SqlSessionTemplate")
-    public SqlSessionTemplate schema1SqlSessionTemplate(@Qualifier("schema1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    SqlSessionTemplate schema1SqlSessionTemplate(@Qualifier("schema1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    	log.info("called");
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
     // Schema 2 DataSource Configuration
     @Bean(name = "schema2DataSource")
     @ConfigurationProperties(prefix = "spring.datasource.schema2")
-    public DataSource schema2DataSource() {
+    DataSource schema2DataSource() {
+    	log.info("called");
         return org.springframework.boot.jdbc.DataSourceBuilder.create().build();
     }
 
     @Bean(name = "schema2SqlSessionFactory")
-    public SqlSessionFactory schema2SqlSessionFactory(@Qualifier("schema2DataSource") DataSource dataSource) throws Exception {
+    SqlSessionFactory schema2SqlSessionFactory(@Qualifier("schema2DataSource") DataSource dataSource) throws Exception {
+    	log.info("called");
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+        sessionFactory.setConfigLocation(new ClassPathResource("/mybatis-config.xml"));
+
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/schema2/*.xml"));
         return sessionFactory.getObject();
     }
 
     @Bean(name = "schema2TransactionManager")
-    public DataSourceTransactionManager schema2TransactionManager(@Qualifier("schema2DataSource") DataSource dataSource) {
+    DataSourceTransactionManager schema2TransactionManager(@Qualifier("schema2DataSource") DataSource dataSource) {
+    	log.info("called");
         return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean(name = "schema2SqlSessionTemplate")
-    public SqlSessionTemplate schema2SqlSessionTemplate(@Qualifier("schema2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    SqlSessionTemplate schema2SqlSessionTemplate(@Qualifier("schema2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    	log.info("called");
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
